d1b85a746f2e8a04f51ec02a0b2ef944bae29144
Shehan Vanderputt <shehan.v@dubliw.com>
2024-03-03 13:04:45 +0530

    Add one test to test AddDoubles method.

--- TestProject/CalculatorTest.cs
+++ TestProject/CalculatorTest.cs
@@ -17,4 +17,17 @@ public class CalculatorTest
         // then
         Assert.Equal(expectedVal, actualVal);
     }
+
+    [Fact]
+    public void Should_Add_Two_Doubles_Return_Doubles()
+    {
+        // given
+        var a = 4.32;
+        var b = 5.22;
+        var expectedVal = 9.54;
+        // when
+        var actualVal = _calc.AddTwoDoubles(a, b);
+        // then
+        Assert.Equal(expectedVal, actualVal, 1);
+    }
 }
